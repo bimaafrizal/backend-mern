7f0dc928a992b7c759a5a9bd2e9497fd0e4fd39a
Bima Afrizal Malna <bimaafrizalmalna@student.uns.ac.id>
2025-03-13 14:29:50 +0700

    Update index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,3 +1,31 @@
-import app from "../src";
+import express from "express";
+import router from "../src/routes/api.ts";
+import bodyParser from "body-parser";
+import db from "../src/utils/database.ts";
+
+async function connectToDatabase() {
+  try {
+    const connection = await db();
+    console.log(connection);
+  } catch (error) {
+    console.error("Database connection error:", error);
+  }
+}
+
+const app = express();
+app.use(bodyParser.json());
+const port = 3000;
+
+router.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.use("/api", router);
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
+
+connectToDatabase();
 
 export default app;
