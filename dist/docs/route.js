02c25d844009823d2091040bdcb75360c2bbd20f
Bima Afrizal Malna <72643530+bimaafrizal@users.noreply.github.com>
2025-04-08 21:23:39 +0700

    debug swagger

--- dist/docs/route.js
+++ dist/docs/route.js
@@ -9,9 +9,17 @@ const swagger_output_json_1 = __importDefault(require("./swagger-output.json"));
 const fs_1 = __importDefault(require("fs"));
 const path_1 = __importDefault(require("path"));
 function docs(app) {
-    const css = fs_1.default.readFileSync(path_1.default.resolve(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"), "utf-8");
-    app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_output_json_1.default, {
-        // customCss: css,
-        customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
-    }));
+    console.log("Setting up swagger");
+    try {
+        //   fs.accessSync(path.resolve(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"));
+        const css = fs_1.default.readFileSync(path_1.default.resolve(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"), "utf-8");
+        app.use("/api-docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_output_json_1.default, {
+            customCss: css,
+            customCssUrl: "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+        }));
+    }
+    catch (error) {
+        console.error("Swagger UI CSS not found:", error);
+        return;
+    }
 }
--- src/docs/route.ts
+++ src/docs/route.ts
@@ -6,15 +6,22 @@ import fs from "fs";
 import path from "path";
 
 export default function docs(app: Express) {
-    const css = fs.readFileSync(path.resolve(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"), "utf-8");
-
-    app.use(
-      "/api-docs",
-      swaggerUi.serve,
-      swaggerUi.setup(swaggerOutput, {
-        // customCss: css,
-        customCssUrl:
-          "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
-      })
-    );
+    console.log("Setting up swagger");
+    try {
+    //   fs.accessSync(path.resolve(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"));
+      const css = fs.readFileSync(path.resolve(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"), "utf-8");
+  
+      app.use(
+        "/api-docs",
+        swaggerUi.serve,
+        swaggerUi.setup(swaggerOutput, {
+          customCss: css,
+          customCssUrl:
+            "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css",
+        })
+      );
+    } catch (error) {
+      console.error("Swagger UI CSS not found:", error);
+      return;
+    }
 }
